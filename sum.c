0683b3c0d06aef2632490f23c64c387c4b03d78c
gavgraham96 <gavgraham96@gmail.com>
2019-04-22 01:37:57 -0400

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+ 
+int main(int array[], int size)
+{
+  int sum = 0;
+    for (c = 1; c < size; c++)
+    {
+        sum+=array[c];
+    }
+    printf("The sum of values is %d.\n", sum);
+    return sum;
+}
