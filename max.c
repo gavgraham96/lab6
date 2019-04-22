e4d0316461457607a9f2bfad718f1728428bc621
gavgraham96 <gavgraham96@gmail.com>
2019-04-22 01:34:35 -0400

    Update max.c

--- max.c
+++ max.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+ 
+int main(int array[], int size)
+{
+  int max = array[0]
+    for (c = 1; c < size; c++)
+    {
+        if (array[c] > max)
+        {
+           max = array[c];
+        }
+    }
+    printf("The maximum value is %d.\n", max);
+    return max;
+}
