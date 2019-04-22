6fa7c046fd3270c4bcb53c61de8d0d2939ce6ca1
gavgraham96 <gavgraham96@gmail.com>
2019-04-22 01:33:19 -0400

    Update min.c

--- min.c
+++ min.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+ 
+int main(int array[], int size)
+{
+  int minimum = array[0]
+    for (c = 1; c < size; c++)
+    {
+        if (array[c] < minimum)
+        {
+           minimum = array[c];
+           location = c+1;
+        }
+    }
+    printf("The minimum value is %d.\n", minimum);
+    return minimum;
+}
