2ad144ddf379980dbe45ca5697a94aba01321bbd
gavgraham96 <gavgraham96@gmail.com>
2019-04-22 01:44:19 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,1 +1,16 @@
-
+int main(int array[], int size)
+{
+  for (int i = 0; i < size; i++)                     //Loop for ascending ordering
+	{
+		for (int j = 0; j < size; j++)             //Loop for comparing other values
+		{
+			if (a[j] > a[i])                //Comparing other array elements
+			{
+				int tmp = a[i];         //Using temporary variable for storing last value
+				a[i] = a[j];            //replacing value
+				a[j] = tmp;             //storing last value
+			}  
+		}
+	}
+  return 0;
+}
